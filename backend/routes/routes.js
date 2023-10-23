b39277d28ce839c9225662583b18b622849c4978
bhavya20428 <bhavya20428@iiitd.ac.in>
2023-10-23 12:59:21 +0530

    Added Cake APIs

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -8,4 +8,10 @@ router.get("/api/seller/cakes", controller.getCakes);
 // Route to add a new cake
 router.post("/api/seller/cakes", controller.addCake);
 
+//Route to update a cake by Id
+router.put("/api/seller/cakes/:cakeId",controller.updateCake);
+
+// Route to delete a cake by ID
+router.delete('/api/seller/cakes/:cakeId',controller.deleteCake); 
+
 export default router;
