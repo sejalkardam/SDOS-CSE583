a8433acc3fdcac04c87a290f87868a8f46fb8124
sejalkardam <sejal20467@iiitd.ac.in>
2023-11-25 18:11:20 +0530

    tests

---
+++ frontend/src/jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+    testEnvironment: 'node',
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+    },
+  };
+
+  
---
+++ frontend/src/pages/sum.js
@@ -0,0 +1,7 @@
+// sum.js
+function add(a, b) {
+    return a + b;
+  }
+  
+module.exports = add;
+  
